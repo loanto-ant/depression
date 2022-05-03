359db6cc0fd5fa540c8c448b4322cb7a1f68753f
loanto-ant <lorenz.filla@gmail.com>
2022-05-03 20:58:57 +0200

    change

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ function showTheD() {
 }
 
 //Schalter auf den gespeicherten Wert setzen
-var cookie = document.cookie.slice(8, -15);
-
+var cookie = document.cookie.split(";");
+cookie = cookie[1];
+cookie.slice(9, -15);
+console.log("cookie after slice: " + cookie);
 switch (cookie) {
    case "true":
       cookie = true;
@@ -27,7 +29,7 @@ switch (cookie) {
       cookie = false;
       break;
 }
-console.log("Savedcookie: " + cookie);
+//console.log("Savedcookie: " + cookie);
 document.getElementById("checkbox").checked = cookie;
 
 //Aktuellen Wert speichern
@@ -42,15 +44,48 @@ function save() {
          break;
    }
    console.log(checked);
+   //Cookie für checked
    document.cookie =
       "checked=" +
       checked +
       ", SameSite=None; Secure; expires=So, 01 Apr 2023 00:00:00 UTC";
-   console.log("document.cookie = " + document.cookie);
+   //Cookie für Zeit
+   var zeit = new Date().getTime();
+   console.log(zeit);
+   //document.cookie = `time=bingchilling, SameSite=None; Secure; expires=So, 01 Apr 2023 00:00:00 UTC`;
+
+   //console.log("document.cookie = " + document.cookie);
 }
 
 //Stats
 //Anzeigen wenn Button klicked
 function showstats() {
    document.getElementById("stats").classList.toggle("stats_show_hide");
+}
+
+function zeit(t) {
+   //Ignoriert einfach das graue
+
+   // t in Millisekunden
+   //Mein eigenes Geburtsdatum zum Austesten
+   //var t = new Date(2006, 8, 14, 0, 0, 0);
+   var t = new Date();
+   var sekunden = t.getSeconds();
+   var minuten = t.getMinutes();
+   var stunden = t.getHours() - 1;
+   if (stunden == -1) {
+      stunden = 0;
+   }
+   var tage = Math.floor(t / 86400000);
+   var output =
+      tage +
+      " Tage, " +
+      stunden +
+      " Stunden, " +
+      minuten +
+      " Minuten, " +
+      sekunden +
+      " Sekunden.";
+
+   return output;
 }
