42d0fcd80e74939fa2a95dc0c7913711c5cd113f
loanto-ant <lorenz.filla@gmail.com>
2022-05-13 22:27:14 +0200

    finish

--- app.js
+++ app.js
@@ -17,11 +17,12 @@ function showTheD() {
 }
 
 //Schalter auf den gespeicherten Wert setzen
+//Wert von document.cookie herausfinden
 var cookie = document.cookie.split(";");
-cookie = cookie[1];
-cookie.slice(9, -15);
-console.log("cookie after slice: " + cookie);
-switch (cookie) {
+let cookie_neu = cookie[0].slice(8, -15);
+console.log("cookie:" + cookie_neu);
+
+switch (cookie_neu) {
    case "true":
       cookie = true;
       break;
@@ -29,12 +30,11 @@ switch (cookie) {
       cookie = false;
       break;
 }
-//console.log("Savedcookie: " + cookie);
 document.getElementById("checkbox").checked = cookie;
 
 //Aktuellen Wert speichern
 function save() {
-   var checked = document.getElementById("checkbox").checked;
+   checked = document.getElementById("checkbox").checked;
    switch (checked) {
       case true:
          checked = false;
@@ -43,16 +43,20 @@ function save() {
          checked = true;
          break;
    }
-   console.log(checked);
+
    //Cookie für checked
    document.cookie =
       "checked=" +
       checked +
       ", SameSite=None; Secure; expires=So, 01 Apr 2023 00:00:00 UTC";
+
    //Cookie für Zeit
    var zeit = new Date().getTime();
-   console.log(zeit);
-   //document.cookie = `time=bingchilling, SameSite=None; Secure; expires=So, 01 Apr 2023 00:00:00 UTC`;
+   //console.log(zeit);
+   document.cookie =
+      "time=" +
+      zeit +
+      ", SameSite=None; Secure; expires=So, 01 Apr 2023 00:00:00 UTC";
 
    //console.log("document.cookie = " + document.cookie);
 }
@@ -61,31 +65,47 @@ function save() {
 //Anzeigen wenn Button klicked
 function showstats() {
    document.getElementById("stats").classList.toggle("stats_show_hide");
+   zeit();
 }
 
-function zeit(t) {
-   //Ignoriert einfach das graue
+function zeit() {
+   //Wert der im cookie gespeicherten Zeit herausfinden
+   var cookie = document.cookie.split(";");
+   let cookie_neu2 = cookie[1];
+   cookie_neu2 = cookie_neu2.match(/\d+/);
+   console.log(cookie_neu2[0]);
 
-   // t in Millisekunden
-   //Mein eigenes Geburtsdatum zum Austesten
-   //var t = new Date(2006, 8, 14, 0, 0, 0);
-   var t = new Date();
-   var sekunden = t.getSeconds();
-   var minuten = t.getMinutes();
-   var stunden = t.getHours() - 1;
-   if (stunden == -1) {
-      stunden = 0;
-   }
-   var tage = Math.floor(t / 86400000);
-   var output =
-      tage +
-      " Tage, " +
-      stunden +
-      " Stunden, " +
-      minuten +
-      " Minuten, " +
-      sekunden +
-      " Sekunden.";
+   //Jetzt-Zeit herausfinden und differenz bilden
+   var jetzt = new Date();
+   var differenz = jetzt.getTime() - cookie_neu2;
 
-   return output;
+   var sekunden_g = Math.floor(differenz / 1000);
+   var s = sekunden_g % 60;
+   var minuten_g = Math.floor(differenz / 60000);
+   var m = minuten_g % 60;
+   var stunden_g = Math.floor(differenz / 3600000);
+   var st = stunden_g % 24;
+   var tage_g = Math.floor(differenz / 86400000);
+
+   //Wenn Null, dann nicht zeigen und wenn null dann Singular
+   if (tage_g == 0) tage_g = "";
+   else if (tage_g == 1) tage_g = tage_g + " day, ";
+   else tage_g = tage_g + " days, ";
+
+   if (st == 0) st = "";
+   else if (st == 1) st = st + " hour, ";
+   else st = st + " hours, ";
+
+   if (m == 1) m = m + " minute and ";
+   else m = m + " minutes and ";
+
+   if (s == 1) s = s + " second. ";
+   else s = s + " seconds. ";
+
+   console.log(checked);
+   if (checked == true) var output_text = "You have been depressed for ";
+   if (checked == false) var output_text = "You have been depression free for ";
+
+   document.getElementById("stats_output").innerHTML =
+      output_text + tage_g + st + m + " minutes and " + s + " seconds.";
 }
